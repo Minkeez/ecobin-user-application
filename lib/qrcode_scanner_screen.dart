624a50befe2aadfd430d3d013ec8e4370e67f976
lnwphee <iam-p@hotmail.com>
2024-05-15 15:11:23 +0700

    qrcode scanner & permission request

--- lib/qrcode_scanner_screen.dart
+++ lib/qrcode_scanner_screen.dart
@@ -1,7 +1,45 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class QRCodeScannerScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+
+class QRCodeScannerScreen extends StatefulWidget {
   const QRCodeScannerScreen({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _QRCodeScannerScreenState createState() => _QRCodeScannerScreenState();
+}
+
+class _QRCodeScannerScreenState extends State<QRCodeScannerScreen> {
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  Barcode? result;
+  QRViewController? controller;
+
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      controller!.pauseCamera();
+    } else if (Platform.isIOS) {
+      controller!.resumeCamera();
+    }
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    this.controller = controller;
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        result = scanData;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,8 +51,32 @@ class QRCodeScannerScreen extends StatelessWidget {
           onPressed: () => Navigator.of(context).pop(),
         ),
       ),
-      body: const Center(
-        child: Text("QR code scanner goes here."),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 5,
+            child: QRView(
+              key: qrKey,
+              onQRViewCreated: _onQRViewCreated,
+              overlay: QrScannerOverlayShape(
+                borderColor: Colors.blue,
+                borderRadius: 10,
+                borderLength: 30,
+                borderWidth: 10,
+                cutOutSize: MediaQuery.of(context).size.width * 0.8,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Center(
+              child: (result != null)
+                  ? Text(
+                      'Barcode Type: ${result!.format}   Data: ${result!.code}')
+                  : const Text('Scan a code'),
+            ),
+          ),
+        ],
       ),
     );
   }
